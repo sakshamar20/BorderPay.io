823af824678b82d2683a63496bfa831edc5d9278
Saksham Arora <74827157+sakshamar20@users.noreply.github.com>
2024-04-28 12:52:47 +0530

    jkgfdijkbg

--- BorderPay/models.py
+++ BorderPay/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+Location =    [('India', 'India'),
+               ('Usa', 'Usa'),
+               ('Canada','Canada'),
+               ('Netherlands', 'Netherlands'),
+               ('Austarlia','Australia')
+               ]
+
+Banks =    [('A', 'A'),
+               ('B', 'B'),
+               ('C','C'),
+               ('D', 'D'),
+               ('E','E')
+               ]
+
+Denominations = [('Rupees','Rupees'),
+                 ('Dollar','Dollar'),
+                 ('Euro','Euro')
+                ]
+
+
 # Create your models here.
+class Employer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Location = models.CharField(max_length=40)
+    Bank = models.CharField(max_length=20)
+    Denomination = models.CharField(
+        max_length=50, choices=departments, default='Cardiologist')
